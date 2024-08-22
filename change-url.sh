c74b511724ee4cdb7be80361c4d0e2651eb623c1
Matteo De Filippis <58430487+matteodf@users.noreply.github.com>
2024-08-22 15:22:04 +0200

    fix: no duplicated lines when changing environment
    Fix #1

--- change-url.sh
+++ change-url.sh
@@ -12,14 +12,27 @@ while ! [ -f /var/www/html/wp-config.php ]; do
     sleep 1
 done
 
-# Check if the custom lines are already present
-if ! grep -q "define('WP_HOME', 'http://localhost:${WORDPRESS_PORT}');" /var/www/html/wp-config.php; then
-    # Insert custom lines before the specific line
+# Define the lines to be added or updated
+wp_home_line="define('WP_HOME', 'http://localhost:${WORDPRESS_PORT}');"
+wp_siteurl_line="define('WP_SITEURL', 'http://localhost:${WORDPRESS_PORT}');"
+remote_media_url_line="define('REMOTE_MEDIA_URL', 'https://${WORDPRESS_URL}');"
+
+# Replace the lines if they exist, otherwise add them
+sed -i.bak -e "/define('WP_HOME',/c\\
+${wp_home_line}" \
+-e "/define('WP_SITEURL',/c\\
+${wp_siteurl_line}" \
+-e "/define('REMOTE_MEDIA_URL',/c\\
+${remote_media_url_line}" /var/www/html/wp-config.php
+
+
+# Check if the custom function is already present
+if grep -q "function ob_replace_home_url" /var/www/html/wp-config.php; then
+    # If the function is present, replace the WORDPRESS_URL in the home_urls array
+    sed -i "/function ob_replace_home_url/,/return \\$content;/s|'http[s]*://[^']*'|'http://${WORDPRESS_URL}'|g" /var/www/html/wp-config.php
+else
+    # Insert custom function before the specific line if it's not present
     sed -i "/\/\* That's all, stop editing! Happy publishing. \*\//i \\
-    define('WP_HOME', 'http://localhost:${WORDPRESS_PORT}');\\n\
-    define('WP_SITEURL', 'http://localhost:${WORDPRESS_PORT}');\\n\
-    define('REMOTE_MEDIA_URL', 'https://${WORDPRESS_URL}');\\n\
-    \\n\
     ob_start('ob_replace_home_url');\\n\
     function ob_replace_home_url(\$content)\\n\
     {\\n\
@@ -27,7 +40,6 @@ if ! grep -q "define('WP_HOME', 'http://localhost:${WORDPRESS_PORT}');" /var/www
             'https://${WORDPRESS_URL}',\\n\
             'http://${WORDPRESS_URL}',\\n\
         );\\n\
-        \\n\
         \\n\
         \$remote_media_url = REMOTE_MEDIA_URL;\\n\
         \\n\
